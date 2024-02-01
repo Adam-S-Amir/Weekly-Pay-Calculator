7ec2f71aa847163058b9f55cdb2422830ab2694c
Adam <64651604+Adam-S-Amir@users.noreply.github.com>
2024-02-01 18:35:39 -0500

    Upload Files

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("\nAdam $ Amir\n");
+        Console.Write("What's your name: ");
+        string? name = Console.ReadLine();
+        Console.Write("Gimme your annual salary: $");
+        if (double.TryParse(Console.ReadLine(), out double annualSalary))
+        {
+            double weeklyPay = annualSalary / 52;
+            Console.WriteLine($"What's good, {name}? Your weekly pay is probably sum like: ${weeklyPay:F2}");
+        }
+        else
+        {
+            Console.WriteLine("Invalid input. Please enter a valid number.");
+        }
+    }
+}
